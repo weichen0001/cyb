b53b06a26f9bc7f96ad287235dae03beda2666b9
weichen0001 <cwshidn@163.com>
2024-06-18 17:04:05 +0800

    1

---
+++ pages/sx/sx.vue
@@ -0,0 +1,40 @@
+<template>
+	<view>
+		
+		<uni-section title="行业" type="line" padding>
+			<view class="example-body">
+				<view class="tag-view">
+				
+			
+					<uni-tag :circle="true" :inverted="inverted" text="标签" type="primary" @click="setInverted" />
+					<uni-tag :circle="true" :inverted="inverted" text="标签" type="primary" @click="setInverted" />
+					<uni-tag :circle="true" :inverted="inverted" text="标签" type="primary" @click="setInverted" />
+					<uni-tag :circle="true" :inverted="inverted" text="标签" type="primary" @click="setInverted" />
+					
+				</view>
+			</view>
+		</uni-section>
+	</view>
+	
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				inverted: false,
+				
+			}
+		},
+		methods: {
+			setInverted() {
+							this.inverted = !this.inverted;
+						},
+			
+		}
+	}
+</script>
+
+<style>
+
+</style>
